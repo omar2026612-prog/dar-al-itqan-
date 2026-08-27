f818cba09d913ce6d7cdc03f0424604cd36fc4e2
omar2026612-prog <omar2026612@gmail.com>
2026-08-27 15:25:57 +0300

    تحديث المشاريع من لوحة التحكم

--- js/projects.js
+++ js/projects.js
@@ -24,5 +24,28 @@ let PROJECTS = [
       "ar": "",
       "en": ""
     }
+  },
+  {
+    "id": "kitchens-mtbhxig843",
+    "category": "kitchens",
+    "title": {
+      "ar": "مطبح عصري تصميم مميز",
+      "en": "مطبح عصري تصميم مميز"
+    },
+    "location": {
+      "ar": "معرض دار الإتقان",
+      "en": "Dar Al Itqan Showroom"
+    },
+    "cover": "images/projects/kitchens-mtbhxig843-cover.jpg",
+    "gallery": [
+      "images/projects/kitchens-mtbhxig843-g1.jpg",
+      "images/projects/kitchens-mtbhxig843-g2.jpg",
+      "images/projects/kitchens-mtbhxig843-g3.jpg",
+      "images/projects/kitchens-mtbhxig843-g4.jpg"
+    ],
+    "description": {
+      "ar": "",
+      "en": ""
+    }
   }
 ];
