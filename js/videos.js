9adacc3d81afb585dc282691b18abaed8534f6a8
omar2026612-prog <omar2026612@gmail.com>
2026-08-27 14:41:50 +0300

    تحديث الفيديوهات من لوحة التحكم

--- js/videos.js
+++ js/videos.js
@@ -11,5 +11,15 @@ let VIDEOS = [
       "en": "دار الاتقان"
     },
     "category": "promo"
+  },
+  {
+    "id": "video-mtbgd08z96",
+    "type": "facebook",
+    "url": "https://www.facebook.com/share/v/18EF8wdX4q/",
+    "title": {
+      "ar": "دار الاتقان",
+      "en": "دار الاتقان"
+    },
+    "category": "promo"
   }
 ];
